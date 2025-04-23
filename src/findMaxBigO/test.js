8d1d8b47e6e98bff90faa6365a36b1721328728a
Hamid Javadi <javadi.business@gmail.com>
2025-04-23 09:59:37 +0300

    Renamed: tests for findMax BigO

--- src/findMaxBigO/test.js
+++ src/findMaxBigO/test.js
@@ -1,6 +1,6 @@
 const { findMax } = require("./solution")
 
-describe('findMax challenge', () => {
+describe('findMax Big(O) challenge', () => {
   test('findMax([1, 5, 2, 9, 3]) => 9', () => {
     expect(findMax([1, 5, 2, 9, 3])).toBe(9)
   })
